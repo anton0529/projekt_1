9d762ec58f17a0cb0a17cfcb372c7742940affb4
Anton <anton123098@gmail.com>
2020-09-17 22:16:23 +0300

    lesson 15

--- script.js
+++ script.js
@@ -9,29 +9,25 @@ const personalMovieBD = {
 
 };
 
-const a = prompt ('Один из просмотреных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt ('Один из просмотреных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+for ( let i = 0; i < 2; i++) {
+    const a = prompt ('Один из просмотреных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
 
-personalMovieBD.movies[a] = b;
-personalMovieBD.movies[c] = d;
-
-console.log(personalMovieBD);
-
-const num = 510;
-
-switch(num) {
-    case 49:
-        console.log('wrong');
-        break;
-    case 100:
-        console.log('wrong');
-        break;
-    case 50:
-        console.log('right');
-        break;
-    default:
-        console.log('wtf');
-        break;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieBD.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
 }
+if (personalMovieBD.count < 10) {
+    console.log('Просмотрено давольно мало фильмов');
+} else if(personalMovieBD.count >= 10 && personalMovieBD.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieBD.count > 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
+console.log(personalMovieBD);
